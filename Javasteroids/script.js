3eef2e298a29aa02874537dd93458e1b25d05811
DaniCF <dancf3@yahoo.es>
2018-11-08 14:18:37 +0100

    obejetos

--- Javasteroids/script.js
+++ Javasteroids/script.js
@@ -1,56 +1,26 @@
 var context = document.getElementById('asteroids').getContext('2d');
 
-//Forma
-var segments = 24;
-var shape = [];
-
-for (var i = 0; i < segments; i++) {
-    shape.push(Math.random() - 0.5);
-}
-
-//Estado
-var radius = 50;
-var noise = 0.2;
-
-var x = context.canvas.width * Math.random();
-var y = context.canvas.heigth * Math.random();
-var angle = 0;
-
-//Movimiento
-
-var x_speed = context.canvas.width * (Math.random() - 0.5);
-var y_speed = context.canvas.heigth * (Math.random() - 0.5);
-var rotation_speed = 2 * Math.PI * (Math.random() - 0.5);
+var asteroids = [
+    new Asteroid(24,50,0.2),
+    new Asteroid(24,50,0.5),
+    new Asteroid(5,50,0.2),
+];
 
 function draw(ctx,guide){
     if(guide){
         draw_grid(ctx);
     }
 
-    ctx.save();
-    ctx.translate(x,y);
-    ctx.rotate(angle);
-    draw_asteroids(ctx, radius, shape, {guide:guide, noise:noise})
-    ctx.restore();
+    asteroids.forEach(function(asteroid){
+        asteroid.draw(context, guide);
+    });
 }
 
 function update(elapsed){
-    if(x - radius + elapsed * x_speed > context.canvas.width){
-        x = -radius;
-    }
-    if(x - radius + elapsed * x_speed < 0){
-        x = context.canvas.width + radius;
-    }
-    if(y - radius + elapsed * y_speed > context.canvas.heigth){
-        y = -radius;
-    }
-    if(y - radius + elapsed * x_speed < 0){
-        y = context.canvas.heigth + radius;
-    }
+    asteroids.forEach(function(asteroid){
+        asteroid.update(elapsed);
+    });
     
-    x += elapsed * x_speed;
-    y += elapsed * y_speed;
-    angle = (angle + elapsed * rotation_speed) % (2 * Math.PI);
 }
 
 var previous, elapsed;
